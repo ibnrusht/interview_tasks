7c733f599594c5645829b002e9f3cd89b39bd3e2
ibnrusht <ibn.rusht@gmail.com>
2022-08-19 09:13:47 +0300

    openCV task

---
+++ project420/first_task.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy
+
+im_path = 'image.png'
+temp_path = 'template.png'
+imgc = cv2.imread(im_path)
+tempc = cv2.imread(temp_path)
+
+img = cv2.cvtColor(imgc, cv2.COLOR_BGR2GRAY)
+temp = cv2.cvtColor(tempc, cv2.COLOR_BGR2GRAY)
+
+w, h = temp.shape[::-1]
+
+res = cv2.matchTemplate(img, temp, cv2.TM_CCOEFF_NORMED)
+
+threshold = 0.5
+
+loc = numpy.where(res >= threshold)
+
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(imgc, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
+
+cv2.imshow('Detected', imgc)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
